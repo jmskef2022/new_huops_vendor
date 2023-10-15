38daa740e4ae7353aed5e4e647239a99cf4b78ca
Dr-who412 <mohamedwaleed412@gmail.com>
2023-10-15 11:39:22 +0300

    this version good with ios and android 2

---
+++ android/app/src/main/kotlin/com/huops/vendors/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huops.vendors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ vendors/android/app/src/main/kotlin/com/huopsapp/vendors/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huopsapp.vendors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
